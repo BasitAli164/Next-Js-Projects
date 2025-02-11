65538012055580474865b0b9766cb3e97a9c6ce3
Basit Ali <opoali911@gmail.com>
2025-02-11 13:58:29 +0500

    fixing the bugs

--- my-app/src/app/page.js
+++ my-app/src/app/page.js
@@ -1,10 +1,7 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-    <h1>Hello Next js</h1>
-    <button className="px-8 py-2 rounded-full bgsky500">click me</button>
-    </div>
+    <>
+    <h1>hello</h1>
+    </>
   );
 }
