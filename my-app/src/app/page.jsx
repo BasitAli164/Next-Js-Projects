90f35e5b308d714fdf35b0cbb65c489ede39ae9a
Basit Ali <opoali911@gmail.com>
2025-02-11 19:01:28 +0500

    understand something new about ammit

--- my-app/src/app/page.jsx
+++ my-app/src/app/page.jsx
@@ -8,6 +8,7 @@ export default function Home() {
   return (
     <>
     <h1>hello</h1>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veniam voluptatibus autem eius cupiditate hic, expedita adipisci facilis explicabo, maxime iusto dolores excepturi corporis maiores quibusdam aliquam doloribus nam necessitatibus inventore.</p>    
     <button className="px-8 py-2 bg-sky-500 rounded-full m-2" onClick={handleClick}>Click me</button>
     </>
   );
