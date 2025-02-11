76c85886d8c6c7a2ffabed8ae14f0ae018d71c6a
Basit Ali <opoali911@gmail.com>
2025-02-11 18:57:19 +0500

    try to understand the core architecture of the next js

--- my-app/src/app/layout.js
+++ my-app/src/app/layout.js
@@ -1,14 +1,14 @@
 import "./globals.css";
 export default function RootLayout({ children }) {
   return (
-    <html  lang="en">
+    <html lang="en">
       <head>
-        
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
         <title>Next js with Basit</title>
       </head>
-      <body>
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
