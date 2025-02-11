8c387959bad8e44b762140d8b5411df41bb0da8b
Basit Ali <opoali911@gmail.com>
2025-02-11 18:43:52 +0500

    changing many things

---
+++ my-app/src/app/components/Hello.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Hello = () => {
+    console.log("form the component")
+  return (
+    <div>
+      <h2>I am basit</h2>
+    </div>
+  )
+}
+
+export default Hello
--- my-app/src/app/page.js
+++ my-app/src/app/page.js
@@ -1,7 +1,14 @@
+'use client';
 export default function Home() {
+  console.log("Hello how are you.....")
+  const handleClick=()=>{
+    console.log("clicking...")
+    alert("Hello")
+  }
   return (
     <>
     <h1>hello</h1>
+    <button className="px-8 py-2 bg-sky-500 rounded-full m-2" onClick={handleClick}>Click me</button>
     </>
   );
 }
