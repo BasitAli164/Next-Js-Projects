3c079e35b44f95794caadb8093f603ca30907a43
Basit Ali <opoali911@gmail.com>
2025-02-13 17:55:22 +0500

    working with the next js

--- my-app/src/app/products/page.jsx
+++ my-app/src/app/products/page.jsx
@@ -1,12 +1,25 @@
-
-const Data = () => {
+const Header = () => {
   return (
-    <div>
-      <h1>Hello Data</h1>
+    <div className={`flex justify-between bg-sky-400 h-12 items-center`} >
+      <div>
+        <h2 className="text-2xl font-bold  uppercase ">Logo</h2>
+      </div>
+      <div>
+        <ul className="flex gap-3 font-bold">
+          <li><a href="#">Home</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Contact</a></li>
+          <li><a href="#">Service</a></li>
+        </ul>
 
+      </div>
+      <div>
+        <input type="text" />
+        <button className="px-4 py-[2px] rounded-full mx-1 bg-sky-800 ">search</button>
+      </div>
     </div>
   )
 }
 
 
-export default Data
+export default Header
