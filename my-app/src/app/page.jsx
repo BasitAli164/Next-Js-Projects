7699b657136b8af5ed9f9e7b6935ce022be2a3aa
Basit Ali <opoali911@gmail.com>
2025-02-13 21:33:04 +0500

    explore many thing in next js today

--- my-app/src/app/page.jsx
+++ my-app/src/app/page.jsx
@@ -2,17 +2,22 @@
 import LearnUseRouter from '@/components/LearnUseRouter';
 import Counter from '../components/counter/page'
 import LearnLink from '@/components/counter/Link';
+import style from '../css/style.module.css'
+import Image from 'next/image';
+import myImage from '../../public/img/picture.png'
 export default function Home() {
   console.log("Hello how are you.....")
  
   return (
     <>
     <h1>hello</h1>
-    <p>Lorem, lkasfld ipsum dolor sit amet consectetur adipisicing elit. Veniam voluptatibus autem eius cupiditate hic, expedita adipisci facilis explicabo, maxime iusto dolores excepturi corporis maiores quibusdam aliquam doloribus nam necessitatibus inventore.</p>    
+    <Image src={myImage} alt='not found'/>
+    <img src={myImage} alt="not" />
+    <p className={style.main}>Lorem, lkasfld ipsum dolor sit amet consectetur adipisicing elit. Veniam voluptatibus autem eius cupiditate hic, expedita adipisci facilis explicabo, maxime iusto dolores excepturi corporis maiores quibusdam aliquam doloribus nam necessitatibus inventore.</p>    
     <button className="px-8 py-2 bg-sky-500 rounded-full m-2" >Click me</button>
     <Counter/>
     {/* <LearnLink/> */}
-    <LearnUseRouter/>
+    {/* <LearnUseRouter/> */}
     </>
   );
 }
--- my-app/src/components/LearnUseRouter.jsx
+++ my-app/src/components/LearnUseRouter.jsx
@@ -1,11 +1,11 @@
-import { useRouter } from "next/navigation "
+import { useRouter } from "next/router"
 const LearnUseRouter = () => {
     const router=useRouter();
     console.log("Learn useRouter hook",router);
   return (
     <div>
       <h1>Use Router Hook</h1>
-      <button>Goo...</button>
+      <button onClick={()=>router.push('/profile')}>Goo...</button>
     </div>
   )
 }
