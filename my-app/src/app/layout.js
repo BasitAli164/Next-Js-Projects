bb2a8379e04cbf88a376b2f87589cbf2a99b3baa
Basit Ali <opoali911@gmail.com>
2025-02-13 19:36:34 +0500

    exploring more things in next js

--- my-app/src/app/layout.js
+++ my-app/src/app/layout.js
@@ -3,7 +3,8 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
         <title>Next js with Basit</title>
       </head>
