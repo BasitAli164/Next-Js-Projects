59f8441249b26c2a98e576c7b95d54e6702f58b7
Basit Ali <opoali911@gmail.com>
2025-02-13 21:00:34 +0500

    working with new thing next js

--- my-app/src/app/page.jsx
+++ my-app/src/app/page.jsx
@@ -1,4 +1,5 @@
 "use client"
+import LearnUseRouter from '@/components/LearnUseRouter';
 import Counter from '../components/counter/page'
 import LearnLink from '@/components/counter/Link';
 export default function Home() {
@@ -10,7 +11,8 @@ export default function Home() {
     <p>Lorem, lkasfld ipsum dolor sit amet consectetur adipisicing elit. Veniam voluptatibus autem eius cupiditate hic, expedita adipisci facilis explicabo, maxime iusto dolores excepturi corporis maiores quibusdam aliquam doloribus nam necessitatibus inventore.</p>    
     <button className="px-8 py-2 bg-sky-500 rounded-full m-2" >Click me</button>
     <Counter/>
-    <LearnLink/>
+    {/* <LearnLink/> */}
+    <LearnUseRouter/>
     </>
   );
 }
--- my-app/src/components/LearnUseRouter.jsx
+++ my-app/src/components/LearnUseRouter.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
-
+import { useRouter } from "next/navigation "
 const LearnUseRouter = () => {
+    const router=useRouter();
+    console.log("Learn useRouter hook",router);
   return (
     <div>
-      
+      <h1>Use Router Hook</h1>
+      <button>Goo...</button>
     </div>
   )
 }
