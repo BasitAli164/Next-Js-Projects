dae7b0b05042249f8ab5ce6789ff2c8cf6379793
Basit Ali <opoali911@gmail.com>
2025-02-14 15:16:36 +0500

    understand how to use the google font in next js

--- my-app/src/app/layout.js
+++ my-app/src/app/layout.js
@@ -1,14 +1,23 @@
+import { Poppins } from 'next/font/google'
 import "./globals.css";
+
+const poppins = Poppins({
+  subsets: ['latin'],
+  weight: '900',
+
+})
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" className={poppins.className}>
       <head>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
         <title>Next js with Basit</title>
       </head>
-      <body>{children}</body>
+      <body >{children}
+        <p>loreasdfjksadj kjdfkjdsa kdjsafkj</p>
+      </body>
     </html>
   );
 }
