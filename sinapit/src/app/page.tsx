786f6632e7d52ffb1e1b9f7261859da7d9bf1598
Basit Ali <opoali911@gmail.com>
2025-02-18 22:54:27 +0500

    wroking on the snippet project

--- sinapit/src/app/page.tsx
+++ sinapit/src/app/page.tsx
@@ -2,29 +2,26 @@ import { Button } from "@/components/ui/button";
 import { prisma } from "@/lib/prisma";
 import Link from "next/link";
 
-const Home =async() => {
-  const snippet=await prisma.snippet.findMany();
+const Home = async () => {
+  const snippet = await prisma.snippet.findMany();
   return (
     <div>
       <h1 className="text-4xl font-bold text-white">Home</h1>
       <div className="flex items-center justify-between">
-        <h1>Snippets</h1>
+        <h1 className="text-white text-xl">Snippets</h1>
         <Link href={`/snippet/new`}>
           <Button>New</Button>
         </Link>
       </div>
-      <div>
-        {
-          snippet?.map((item)=>(
-            <div className="flex justify-between gap-y-10" key={item.id}>
-              <h1 className="text-white text-xl  bg-slate-300">{item.title}</h1>
-              <Button className="mt-10">View</Button>
-              
-            </div>
 
-          ))
-        }
-      </div>
+      {snippet?.map((item) => (
+        <div className="flex justify-between items-center bg-slate-800 p-y-[5px] rounded-md my-[14px]" key={item.id}>
+          <h1 className="text-white text-xl ml-2 ">{item.title}</h1>
+          <Link href={`/snippet/${item.id}`}>
+            <Button variant={'link'} className="mt-5 text-white ">View</Button>
+          </Link>
+        </div>
+      ))}
     </div>
   );
 };
