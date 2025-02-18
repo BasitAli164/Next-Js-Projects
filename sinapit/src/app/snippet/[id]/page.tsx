05b6f9faeac9a4362ebc2a235b171003815f46b4
Basit Ali <opoali911@gmail.com>
2025-02-18 23:24:52 +0500

    working on the snippet project

--- sinapit/src/app/snippet/[id]/page.tsx
+++ sinapit/src/app/snippet/[id]/page.tsx
@@ -1,11 +1,38 @@
-import React from 'react'
+import { Button } from "@/components/ui/button";
+import { prisma } from "@/lib/prisma";
+import React from "react";
 
-const ReviweSnippet = () => {
+const ReviweSnippet = async ({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) => {
+  let id = parseInt((await params).id);
+  const snippet = await prisma.snippet.findUnique({
+    where: {
+      id,
+    },
+  });
+
+  if (!snippet) return <h1> There are no snippt</h1>;
   return (
     <div>
-      <h2 className='text-white'>Hi hello</h2>
+      <div className="flex justify-between">
+        <h1 className="text-white text-xl font-semibold">{snippet?.title}</h1>
+        <div className="flex gap-5">
+          <Button size={"lg"}>Edit</Button>
+          <Button variant={"destructive"} size={"lg"}>
+            Delete
+          </Button>
+        </div>
+      </div>
+      <div className="bg-slate-700 mt-3 p-5 rounded-md">
+        <pre>
+            <code className="text-white">{snippet.code}</code>
+        </pre>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ReviweSnippet
+export default ReviweSnippet;
