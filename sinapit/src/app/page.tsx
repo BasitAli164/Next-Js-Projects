c94b7584a1d8cb7f9c131dcd780477b3c651e7ec
Basit Ali <opoali911@gmail.com>
2025-02-18 22:40:01 +0500

    working on the snappit mini porject

--- sinapit/src/app/page.tsx
+++ sinapit/src/app/page.tsx
@@ -1,7 +1,9 @@
 import { Button } from "@/components/ui/button";
+import { prisma } from "@/lib/prisma";
 import Link from "next/link";
 
-const Home = () => {
+const Home =async() => {
+  const snippet=await prisma.snippet.findMany();
   return (
     <div>
       <h1 className="text-4xl font-bold text-white">Home</h1>
@@ -10,6 +12,18 @@ const Home = () => {
         <Link href={`/snippet/new`}>
           <Button>New</Button>
         </Link>
+      </div>
+      <div>
+        {
+          snippet?.map((item)=>(
+            <div className="flex justify-between gap-y-10" key={item.id}>
+              <h1 className="text-white text-xl  bg-slate-300">{item.title}</h1>
+              <Button className="mt-10">View</Button>
+              
+            </div>
+
+          ))
+        }
       </div>
     </div>
   );
