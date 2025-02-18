558e85ddf914210f95fa9162b053b7ec9c93b0b0
Basit Ali <opoali911@gmail.com>
2025-02-18 10:23:53 +0500

    exploring new technologies in web field

--- sinapit/src/app/page.tsx
+++ sinapit/src/app/page.tsx
@@ -1,9 +1,14 @@
+import { Button } from "@/components/ui/button";
 import exp from "constants";
 
 const Home = () => {
   return (
-    <div className="w-full bg-gray-400 ">
-    <h1 className="font-bold text-5xl text-white text-center p-10 relative z-50">Hello</h1>
+    <div className="w-full bg-gray-400  justify-center items-center">
+    <h1 className="font-bold text-5xl text-white text-center  relative z-50">Hello</h1>
+    <div className="flex justify-center items-start ">
+    <Button variant={"basit"}>Something New</Button>
+
+    </div>
     </div>
   )
 };
