163a934b0f7f382ad8af182eea6a2ca4f1274a2f
Basit Ali <opoali911@gmail.com>
2025-02-19 14:50:01 +0500

    working the snippet project in next js

--- sinapit/src/app/snippet/[id]/edit/page.tsx
+++ sinapit/src/app/snippet/[id]/edit/page.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const EditSnippet = () => {
+const EditSnippet = ({params}:{params:Promise<{id:string}>}) => {
   return (
     <div>
         <h1 className='text-white font-bold text-3xl'>Hello Snippets</h1>
