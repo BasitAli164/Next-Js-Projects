1b5c97600dfe5562e152eb811e797c4df595c0c7
Basit Ali <opoali911@gmail.com>
2025-02-21 13:17:01 +0500

    the header is complete and start the about page

---
+++ portfolio/src/app/about/page.jsx
@@ -0,0 +1,12 @@
+'use client'
+const AboutPage = () => {
+  return (
+    <div  className="w-full px-[12%] py-10 scroll-mt-20">
+        <h1>Introduction</h1>
+        <h2>About Me</h2>
+      
+    </div>
+  )
+}
+
+export default AboutPage
--- portfolio/src/app/page.jsx
+++ portfolio/src/app/page.jsx
@@ -1,10 +1,12 @@
 import Header from '@/components/Header'
 import React from 'react'
+import AboutPage from './about/page'
 
 const Home = () => {
   return (
     <div>
       <Header/>
+      <AboutPage/>
     </div>
   )
 }
