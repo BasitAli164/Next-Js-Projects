04eabd318e0fe0449bb91dcf3386fc175d257b8e
Basit Ali <opoali911@gmail.com>
2025-02-21 10:34:01 +0500

    working in the navbar component

--- portfolio/src/components/Navbar.jsx
+++ portfolio/src/components/Navbar.jsx
@@ -1,17 +1,18 @@
 import Link from "next/link";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArrowRight,faMoon,faSun,faBars } from "@fortawesome/free-solid-svg-icons";
+import { faArrowRight,faMoon,faSun,faBars,faClose } from "@fortawesome/free-solid-svg-icons";
 import { Button } from "./ui/button";
 const Navbar = () => {
   const arrowIcon = <FontAwesomeIcon icon={faArrowRight} />;
   const moonIcon=<FontAwesomeIcon icon={faMoon}/>
   const sunIcon=<FontAwesomeIcon icon={faSun}/>
   const barIcon=<FontAwesomeIcon icon={faBars}/>
+  const crossIcon=<FontAwesomeIcon icon={faClose}/>
   return (
     <>
       <nav className="w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between z-50">
         <Link href="#top">
-          <h1 className="text-4xl  relative"><span className="absolute bottom-5  left-[-25px]">...</span>Basit...</h1>
+          <h1 className="text-4xl  relative"><span className="absolute bottom-6  left-[-6px] font-bold">.</span>Basit <span className="absolute bottom-[3.5px] right-[-6px] font-bold">.</span></h1>
         </Link>
         <ul className="hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3 bg-white shadow-md bg-opacity-25">
           <li>
@@ -53,6 +54,36 @@ const Navbar = () => {
           </Link>
           <Button className="block md:hidden ml-3" variant="destructive">{barIcon}</Button>
         </div>
+        {/* ------------ Mobile Menu ------------*/}
+        <ul className="md:hidden flex flex-col gap-4 py-20 px-10 fixed -right-64 top-0 bottom-0 z-50 w-64 h-screen bg-rose-50 transition duration-500">
+          <div className="absolute right-6 top-6"><li className="w-5 cursor-pointer">{crossIcon}</li></div>
+          <li>
+            <Link href="/" className="font-Ovo">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link href="/about" className="font-Ovo">
+              About me
+            </Link>
+          </li>
+          <li>
+            <Link href="/service" className="font-Ovo">
+              Services
+            </Link>
+          </li>
+          <li>
+            <Link href="/skill" className="font-Ovo">
+              My Skills
+            </Link>
+          </li>
+          <li>
+            <Link href="/contact" className="font-Ovo">
+              Contact me
+            </Link>
+          </li> 
+
+        </ul>
       </nav>
     </>
   );
