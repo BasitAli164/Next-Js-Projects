d54e61d9ae0e5a724f80372529a3e3b48b86281c
Basit Ali <opoali911@gmail.com>
2025-02-23 02:10:06 +0500

    last commit for toddays

--- portfolio/src/app/contact/page.jsx
+++ portfolio/src/app/contact/page.jsx
@@ -1,9 +1,10 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
-import React from "react";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 const ContactPage = () => {
+  const rightArrowIcon = <FontAwesomeIcon icon={faArrowRight} />;
   return (
     <div
       className='full px-[12%] py-10 scroll-mt-10 bg-[url("/bgimage.png")] bg-no-repeat bg-center
@@ -16,11 +17,33 @@ const ContactPage = () => {
       </p>
       <form className="max-w-2xl mx-auto">
         <div className="flex gap-6 mt-10 mb-8">
-          <Input type="text" placeholder="Enter your name" required className=" p-6 outline-none border-[0.5px] border-gray-400 rounded-md bg-white text-lg placeholder:text-lg font-Ovo"/>
-          <Input type="email" placeholder="Enter your email" required className=" p-6 outline-none border-[0.5px] border-gray-400 rounded-md bg-white text-lg placeholder:text-lg font-Ovo"/>
+          <Input
+            type="text"
+            placeholder="Enter your name"
+            required
+            className=" p-6 outline-none border-[0.5px] border-gray-400 rounded-md bg-white text-lg placeholder:text-lg font-Ovo"
+          />
+          <Input
+            type="email"
+            placeholder="Enter your email"
+            required
+            className=" p-6 outline-none border-[0.5px] border-gray-400 rounded-md bg-white text-lg placeholder:text-lg font-Ovo"
+          />
         </div>
-        <Textarea rows='6' placeholder="Enter your message..." required className=" p-6 outline-none border-[0.5px] border-gray-400 rounded-md bg-white mb-6 text-lg placeholder:text-lg font-Ovo"/>  
-        <Button type="submit" className="py-6 px-10 w-max flex items-center justify-between gap-2 bg-black/80 text-white rounded-full mx-auto hover:bg-black transition duration-500 text-lg">Submit now</Button>
+        <Textarea
+          rows="6"
+          placeholder="Enter your message..."
+          required
+          className=" p-6 outline-none border-[0.5px] border-gray-400 rounded-md bg-white mb-6 text-lg placeholder:text-lg font-Ovo"
+        />
+        <Button
+          type="submit"
+          className="py-6 px-9 w-max flex items-center justify-between gap-2 bg-black/80 text-white rounded-full mx-auto hover:bg-black transition duration-500 text-lg"
+        >
+          Submit now
+          <span className="text-white ">{rightArrowIcon}</span>
+        </Button>
+        <p className="text-lg mt-5">Sending...</p>
       </form>
     </div>
   );
