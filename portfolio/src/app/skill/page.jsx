191c0ed2c55c979b26abc1e39cec31519a67f882
Basit Ali <opoali911@gmail.com>
2025-02-23 01:09:59 +0500

    the skill section is almost complete

--- portfolio/src/app/skill/page.jsx
+++ portfolio/src/app/skill/page.jsx
@@ -1,8 +1,25 @@
+import { mySkills } from '@/assets/assest'
+import Image from 'next/image'
 import React from 'react'
 
 const SkillPage = () => {
   return (
-    <div>
+    <div className='full px-[12%] py-10 scroll-mt-10'>
+      <h2 className='text-center text-5xl font-Ovo'>My Skill</h2>
+      <p className='text-center max-w-2xl mx-auto mt-5 mb-12 font-Ovo text-lg'>I am a full stack developer with the experties in these skill or stack which are the top demanded skills in industry with 7 month experience </p>
+      <div className='grid grid-cols-5 gap-8 my-10 items-center justify-center'>
+        {
+          mySkills.map(({icon,title,description},index)=>(
+            <div key={index} className='border border-gray-400 rounded-lg flex flex-col justify-center items-center w-48 h-56 hover:shadow-black cursor-pointer hover:bg-lightHover hover:-translate-y-1 transition duration-500'>
+              <Image src={icon} alt={title} className='w-14 rounded-md'/> <br />
+              <h3 className='text-lg my-4 text-gray-700'>{title}</h3>
+              <p className='text-sm  text-gray-600  leading-6 text-center'>{description}</p>
+
+            
+            </div>
+          ))
+        }
+      </div>
       
     </div>
   )
--- portfolio/src/assets/assest.js
+++ portfolio/src/assets/assest.js
@@ -1,15 +1,18 @@
-import codeIcon from '@/assets/iconImage/code-icon.png';
-import codeIconDark from '@/assets/iconImage/code-icon-dark.png';
-import educIcon from '@/assets/iconImage/edu-icon.png';
-import educIconDark from '@/assets/iconImage/edu-icon-dark.png';
-import projectIcon from '@/assets/iconImage/project-icon.png';
-import projectIconDark from '@/assets/iconImage/project-icon-dark.png';
-import gitIcon from '@/assets/toolImages/git.png';
-import vscodeIcon from '@/assets/toolImages/vscode.png';
-import mongodb from '@/assets/toolImages/mongodb.png';
-import postman from '@/assets/toolImages/postman.png'
-
-
+import codeIcon from "@/assets/iconImage/code-icon.png";
+import codeIconDark from "@/assets/iconImage/code-icon-dark.png";
+import educIcon from "@/assets/iconImage/edu-icon.png";
+import educIconDark from "@/assets/iconImage/edu-icon-dark.png";
+import projectIcon from "@/assets/iconImage/project-icon.png";
+import projectIconDark from "@/assets/iconImage/project-icon-dark.png";
+import gitIcon from "@/assets/toolImages/git.png";
+import vscodeIcon from "@/assets/toolImages/vscode.png";
+import mongodb from "@/assets/toolImages/mongodb.png";
+import postman from "@/assets/toolImages/postman.png";
+import frontendTech from '@/assets/skillImage/frontend.png';
+import uiTech from '@/assets/skillImage/ui2.png';
+import backendTech from '@/assets/skillImage/backend.jpg';
+import fullTech from '@/assets/skillImage/full.jpg';
+import dbTech from '@/assets/skillImage/db.jpg';
 export const infoList = [
   {
     icon: codeIcon,
@@ -30,4 +33,34 @@ export const infoList = [
     description: "Built more than 5 projects",
   },
 ];
-export const toolsDataLight=[vscodeIcon,gitIcon,mongodb,postman]
+export const toolsDataLight = [vscodeIcon, gitIcon, mongodb, postman];
+export const mySkills = [
+ 
+  {
+    icon:frontendTech,
+    title: "Frontend Technology",
+    description: "HTML5, CSS, Javascript, Typescript, and React",
+   
+  },
+  {
+    icon:uiTech,
+    title: "UI Technology",
+    description: "Tailwind CSS, SCSS, Bootstrap, Shadcn, Mui, and Mantine(exploring)",
+   
+  },
+  {
+    icon:backendTech,
+    title: "Backend Technology",
+    description:"Node js, Express js",
+  },
+  {
+    icon:dbTech,
+    title: "Database Technology",
+    description: "Mongodb(mongoose), Sqlite(prisma)  ",
+  },
+  {
+    icon:fullTech,
+    title: "Web Framewrok",
+    description: "Next js ",
+  },
+];
