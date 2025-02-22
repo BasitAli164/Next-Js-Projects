68b3b75da1cfbd7ef151e2115ac6ddb844b2fa1f
Basit Ali <opoali911@gmail.com>
2025-02-23 01:38:10 +0500

    know start the contact

--- portfolio/src/app/contact/page.jsx
+++ portfolio/src/app/contact/page.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
 
 const ContactPage = () => {
   return (
-    <div>
-      <h1>Contact Page </h1>
+    <div
+      className='full px-[12%] py-10 scroll-mt-10 bg-[url("/bgimage.png")] bg-no-repeat bg-center
+    bg-[length:90%_auto]'
+    >
+      <h2 className="text-center text-5xl font-Ovo">Get in Touch</h2>
+      <p className="text-center max-w-2xl mx-auto mt-5 mb-12 font-Ovo text-lg">
+        I'd love to hear from you! If you have any questions, comments, or
+        feedback, please use the form below.{" "}
+      </p>
     </div>
-  )
-}
+  );
+};
 
-export default ContactPage
+export default ContactPage;
