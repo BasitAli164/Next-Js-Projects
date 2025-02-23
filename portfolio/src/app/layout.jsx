654d219a761e126f9a2f8fb7c7023d47cdd4452f
Basit Ali <opoali911@gmail.com>
2025-02-23 19:09:23 +0500

    working on the dark theme logics

--- portfolio/src/app/layout.jsx
+++ portfolio/src/app/layout.jsx
@@ -3,7 +3,7 @@ import {Ovo,Outfit} from 'next/font/google'
 import "./globals.css";
 import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { faL } from '@fortawesome/free-solid-svg-icons';
 
 const outfit=Outfit({  
@@ -25,6 +25,18 @@ const ovo=Ovo({
 
 export default function RootLayout({ children }) {
   const [isDarkMood,setIsDarkMood]=useState(true);
+  useEffect(()=>{
+    
+  },[])
+  useEffect(()=>{
+    if(isDarkMood){
+      document.documentElement.classList.add('dark');
+      localStorage.theme='dark';
+    }else{
+      document.documentElement.classList.remove('dark');
+      localStorage.theme='';
+    }
+  },[isDarkMood])
   return (
     <html lang="en" className=' '>
       <body className={`${outfit.className} ${ovo.className} antialiased leading-6 overflow-x-hidden scroll-smooth dark:bg-darkTheme dark:text-white` }>
