f73a366979d024fc8051625a4c898e3c16674e45
Basit Ali <opoali911@gmail.com>
2025-02-23 19:14:27 +0500

    exploring new thing

--- portfolio/src/app/page.jsx
+++ portfolio/src/app/page.jsx
@@ -4,11 +4,19 @@ import React, { useEffect, useState } from 'react'
 import AboutPage from './about/page'
 import SkillPage from './skill/page'
 import ContactPage from './contact/page'
+import { faL } from '@fortawesome/free-solid-svg-icons'
 
 
 const Home = () => {
   const [isDarkMood,setIsDarkMood]=useState(true);
   useEffect(()=>{
+    if(localStorage.theme==='dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme:dark)').matches)){
+      setIsDarkMood(true)
+
+    }else{
+      setIsDarkMood(false)
+
+    }
     
   },[])
   useEffect(()=>{
