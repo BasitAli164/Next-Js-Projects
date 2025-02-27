fed78ea7ca6b44dcdb479c22eb8cab2145342877
Basit Ali <opoali911@gmail.com>
2025-02-27 21:55:41 +0500

    again start the portfolio project

--- portfolio/src/app/not-found.jsx
+++ portfolio/src/app/not-found.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 
-const ErrorPage = () => {
+const ErrorPage = async({params}) => {
+    console.log("QIE",await params)
+    // const para=await params;
   return (
     <div className='h-screen w-full flex justify-center items-center'>
-      <h1>Not  Found</h1>
+      <h1> Page Not  Found </h1>
     </div>
   )
 }
