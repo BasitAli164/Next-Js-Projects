1b037f855c6de1818bf877885e8767abd39d4c21
Basit Ali <opoali911@gmail.com>
2025-04-04 15:09:01 +0500

    create basic setup

--- my-app/src/app/layout.js
+++ my-app/src/app/layout.js
@@ -1,15 +1,18 @@
 import "./globals.css";
-
+import header from "@/component/header";
+import Footer from "@/component/footer";
 export default function RootLayout({ children }) {
   return (
     <html lang="en" >
       <head>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-
-        <title>Next js with Basit</title>
+        <meta name="Mountain Travels  Pakistan" content="This is the best site to reach mountain travelers"/>
+        <link></link>
+        <title>Mountain Travels Agency</title>
       </head>
       <body>
+        <Headers/>
         
         {children}
       
