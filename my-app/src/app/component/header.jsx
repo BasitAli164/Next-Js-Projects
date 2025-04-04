be62d7608d4ecf26304d5553e669d36fe36f2689
Basit Ali <opoali911@gmail.com>
2025-04-04 15:36:24 +0500

    complete the header and footer of dashboard of tourism

--- my-app/src/app/component/header.jsx
+++ my-app/src/app/component/header.jsx
@@ -3,7 +3,7 @@ import {Bell,User} from "lucide-react"
 
 const Header = () => {
   return (
-    <header className='flex justify-between items-center border h-20 bg-gray-300 p-5 '>
+    <header className='flex justify-between items-center border h-20 bg-[#e9e9e9] p-5 '>
       <div>
         <h1 className='text-4xl font-semibold'>Mountain Travels Pakistan</h1>
         
