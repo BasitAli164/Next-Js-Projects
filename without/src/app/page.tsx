a1b24e5a64942d81003f68bc61e21af72cd2de34
Basit Ali <opoali911@gmail.com>
2025-04-26 10:07:56 +0500

    now making a button when click there then navigate into another on

--- without/src/app/page.tsx
+++ without/src/app/page.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
 
+
 const Home = () => {
+  const handleFunc=()=>{
+  }
   return (
-    <div>
-      <h1>Home Page</h1>
+    <div className='flex justify-center items-center w-full h-screen border-[10px] border-white'>
+      <h1 className='text-4xl text-red-600'>Home Page</h1>
+      <button onClick={handleFunc}>Click Me</button>
     </div>
   )
 }
