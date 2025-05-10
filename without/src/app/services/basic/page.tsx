ebe36ce3e826e608b78bbe878684583fd08c71e1
Basit Ali <opoali911@gmail.com>
2025-05-10 10:07:37 +0500

    now exploring the nested routing

---
+++ without/src/app/services/basic/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const WebDev = () => {
+  return (
+    <div>
+      <p>Html -----80%</p>
+      <p>Css -----60%</p>
+      <p>JS -----70%</p>
+    </div>
+  )
+}
+
+export default WebDev
