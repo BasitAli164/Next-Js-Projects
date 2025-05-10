d27561d8d7ba9d5bfbc8e3589886c90dd8acb4c6
Basit Ali <opoali911@gmail.com>
2025-05-10 14:25:06 +0500

    now going to make a button in home page

--- without/src/app/blogs/page.tsx
+++ without/src/app/blogs/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const BlogPage = () => {
   return (
     <div>
-        <h1 className=''>Technical Agency....</h1>
+        <h1 className='font-bold text-5xl'>Technical Agency....</h1>
         <ul >
             <li className='text-3xl font-light'>React js</li>
             <li className='text-3xl font-light'>Express js</li>
