82fa136644ea57c58d5d9db0aa4b445a24045465
Basit Ali <opoali911@gmail.com>
2025-05-10 14:49:41 +0500

    make two blogs

--- my-app/src/app/page.jsx
+++ my-app/src/app/page.jsx
@@ -1,10 +1,14 @@
 import Hero from '@/components/hero'
+import Link from 'next/link'
 import React from 'react'
 
 const Home = () => {
   return (
     <div className='h-screen w-full flex justify-center items-center'>
-      <h1>Home page</h1>
+      <h1>Technical Agency...</h1>
+      <Link href={'/blog'}>Blog</Link>
+      <Link href={'/about'}>About</Link>
+ 
     </div>
   )
 }
