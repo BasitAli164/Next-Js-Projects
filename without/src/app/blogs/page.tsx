2deb2dfe3098cd3645df0437a62a36c49ff891cb
Basit Ali <opoali911@gmail.com>
2025-05-10 14:30:21 +0500

    know working with the searchparams

--- without/src/app/blogs/page.tsx
+++ without/src/app/blogs/page.tsx
@@ -1,13 +1,14 @@
+import Link from 'next/link'
 import React from 'react'
 
 const BlogPage = () => {
   return (
-    <div>
-        <h1 className='font-bold text-5xl'>Technical Agency....</h1>
-        <ul >
-            <li className='text-3xl font-light'>React js</li>
-            <li className='text-3xl font-light'>Express js</li>
-            <li className='text-3xl font-light'>Node js</li>
+    <div className='w-[600px] h-[200px] flex flex-col justify-center items-center bg-violet-200'>
+        <h1 className='font-bold text-5xl text-black'>Technical Agency....</h1>
+        <ul  >
+            <Link href={'/blogs/'} className='text-3xl font-light text-black'>React js</Link>
+            <Link href={'/blogs/'} className='text-3xl font-light text-black'>Express js</Link>
+            <Link href={'/blogs/'} className='text-3xl font-light text-black'>Node js</Link>
         </ul>
       
     </div>
