ff021fc65afa9b6407931c3092b27ac110b527d4
Basit Ali <opoali911@gmail.com>
2025-05-10 09:15:56 +0500

    start to working again with anurag procoder

--- without/src/app/page.tsx
+++ without/src/app/page.tsx
@@ -1,15 +1,18 @@
 'use client'
+
+import { useRouter } from 'next/navigation'
 import React from 'react'
-import Router from 'next/router'
 
 const Home = () => {
+  const router=useRouter()
   const handleFunc=()=>{
-    Router.push('/about')
+    router.push('/about')
+    
   }
   return (
     <div className='flex justify-center items-center w-full h-screen border-[10px] border-white'>
       <h1 className='text-4xl text-red-600'>Home Page</h1>
-      <button onClick={handleFunc}>Click Me</button>
+      <button onClick={handleFunc} className='px-8 py-2 ml-4 rounded-full bg-gray-100 text-black'>Click Me</button>
     </div>
   )
 }
