178fd9470de79fda6000e5d5cfabe1e57005c805
Basit Ali <opoali911@gmail.com>
2025-05-10 14:24:33 +0500

    create the blog page

--- without/src/app/blogs/page.tsx
+++ without/src/app/blogs/page.tsx
@@ -3,6 +3,12 @@ import React from 'react'
 const BlogPage = () => {
   return (
     <div>
+        <h1 className=''>Technical Agency....</h1>
+        <ul >
+            <li className='text-3xl font-light'>React js</li>
+            <li className='text-3xl font-light'>Express js</li>
+            <li className='text-3xl font-light'>Node js</li>
+        </ul>
       
     </div>
   )
