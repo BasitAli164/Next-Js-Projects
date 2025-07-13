2381d62bcaea7c0ce6be33d92378f9daa48dd5da
Basit Ali <opoali911@gmail.com>
2025-07-13 17:49:46 +0500

    create first component in next js

--- my-app/src/app/page.jsx
+++ my-app/src/app/page.jsx
@@ -1,20 +1,11 @@
+import Template from '@/component/basicTemplate'
 import React from 'react'
 
 const Page = () => {
   return (
-    <>
-    <header className='bg-black h-24 text-center text-4xl'>Header</header>
-    <main className='bg-red-600 h-[538px] text-center text-4xl grid grid-cols-3'>
-      <section className='bg-yellow-700'>
-hi
-      </section>
-      <section className='bg-violet-700 '>
-        hell
-      </section>
-      how
-    </main>
-    <footer className='bg-green-800 h-24 text-center text-4xl'>Footer</footer>
-    </>
+    <div>
+      <Template/>
+    </div>
   )
 }
 
