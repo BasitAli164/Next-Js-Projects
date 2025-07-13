58d2dd6f0dd3e1248b79e0d6e83fa31d73ff7319
Basit Ali <opoali911@gmail.com>
2025-07-13 17:08:39 +0500

    create new next js app

--- my-app/src/app/page.jsx
+++ my-app/src/app/page.jsx
@@ -1,17 +1,8 @@
-import Link from 'next/link'
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 
 const Home = () => {
-  return (
-    <div className='h-screen w-full'>
-      <h1>Technical Agency...</h1>
-      <div>
-        <Link href={'/blog'}>Blog</Link><br />
-      <Link href={'/about'}>About</Link>
-      </div>
- 
-    </div>
-  )
-}
+  return <></>;
+};
 
-export default Home
+export default Home;
