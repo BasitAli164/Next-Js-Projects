7f0be3fda89c62a9742837491f63f462f3e65510
Basit Ali <opoali911@gmail.com>
2025-07-13 17:13:38 +0500

    working in next js

--- my-app/src/app/page.jsx
+++ my-app/src/app/page.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 
 const Page = () => {
   return (
-    <div>
+    <div className=' bg-gray-600 flex justify-center items-center max-w-[400px]  '>
+      <input type="date" className='border-[10px] p-2 px-10' />
       
     </div>
   )
