b5a518035525fd4fffc6e2699481e282440e2ec0
Basit Ali <opoali911@gmail.com>
2025-07-13 17:45:22 +0500

    create basic template in next js

--- my-app/src/app/page.jsx
+++ my-app/src/app/page.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
 
-const page = () => {
+const Page = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+    <header className='bg-black h-24 text-center text-4xl'>Header</header>
+    <main className='bg-red-600 h-[538px] text-center text-4xl grid grid-cols-3'>
+      <section className='bg-yellow-700'>
+hi
+      </section>
+      <section className='bg-violet-700 '>
+        hell
+      </section>
+      how
+    </main>
+    <footer className='bg-green-800 h-24 text-center text-4xl'>Footer</footer>
+    </>
   )
 }
 
-export default page
+export default Page
