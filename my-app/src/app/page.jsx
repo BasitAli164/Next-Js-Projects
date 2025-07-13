e5bd7c1b3e35de8e5ab62fe035ff795be26906b9
Basit Ali <opoali911@gmail.com>
2025-07-13 17:17:42 +0500

    make a component in next js

--- my-app/src/app/page.jsx
+++ my-app/src/app/page.jsx
@@ -2,10 +2,9 @@ import React from 'react'
 
 const Page = () => {
   return (
-    <div className=' bg-gray-600 flex justify-center items-center max-w-[400px]  '>
-      <input type="date" className='border-[10px] p-2 px-10' />
-      
-    </div>
+    <>
+   
+    </>
   )
 }
 
